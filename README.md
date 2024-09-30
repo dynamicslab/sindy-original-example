bc78a60f8bcedd78b36b6ee5c29911b426a0cae9
Jake Stevens-Haas <37048747+Jacob-Stevens-Haas@users.noreply.github.com>
2024-09-29 17:23:24 -0700

    Add documentation scripts/instructions

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # pysindy-example
-Template for example notebooks to include in pysindy's docs
+Template for example notebooks to include in pysindy's docs.  To make a new
+example, follow the instructions on pysindy's README.
+
+To add a new set of examples:
+
+1. Clone this repository, changing the project name and description in docs/conf.py
+1. Save the notebooks in examples/
+1. Add references to the toctree in examples/index.rst
+1. Add notebook run dependencies to examples/requirements.txt.
+1. Build the documentation and verify it builds without warning or error:
+```
+pip install -r requirements-docs.txt
+sphinx-build -W docs/source docs/build
+```
+1. Add and commit build result
+
+Other dependency methods, e.g. pyproject.toml, are acceptable.  You will need
+to modify the appropriate CI step
---
+++ docs/source/conf.py
@@ -0,0 +1,45 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+from pathlib import Path
+from shutil import copy, rmtree, copytree
+
+from sphinx.application import Sphinx
+
+project = 'pysindy-example'
+copyright = '2024, Jacob Stevens-Haas'
+author = 'Jacob Stevens-Haas'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "nbsphinx",
+    "IPython.sphinxext.ipython_console_highlighting",
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_rtd_theme"
+html_static_path = ['_static']
+
+def setup(app: Sphinx):
+    """Copy files from repo/examples to repo/docs/_sources/examples"""
+    docs = Path(__file__).absolute().parents[1]
+    examples_from =  docs.parent / "examples"
+    examples_to = docs / "source" / "examples"
+    if examples_to.exists():
+        rmtree(examples_to)
+    copytree(examples_from, examples_to)
